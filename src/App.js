f10f1642562a13401d9fbd5b3f8ede2e1e26e8dd
Sudhanva Hebbar <hebbarsjk@gmail.com>
2020-10-06 18:08:13 +0530

    removed usestate and useeffect

--- src/App.js
+++ src/App.js
@@ -2,18 +2,18 @@ import React, { useEffect, useState } from "react";
 import "./App.css";
 
 const App = () => {
-  const [results, setResults] = useState("");
+  // const [results, setResults] = useState("");
 
-  useEffect(() => {
-    getResults();
-  }, []);
+  // useEffect(() => {
+  //   getResults();
+  // }, []);
 
-  const getResults = async () => {
-    const response = await fetch(`https://covid19-api.com/country/all`);
-    const data = await response.json();
-    setResults(data.hits);
-    console.log(data.hits);
-  };
+  // const getResults = async () => {
+  //   const response = await fetch(`https://covid19-api.com/country/all`);
+  //   const data = await response.json();
+  //   setResults(data.hits);
+  //   console.log(data.hits);
+  // };
 
   return (
     <div className="App">
